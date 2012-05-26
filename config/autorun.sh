cdadb20768de2d329cd4e13bcd3ecf74b781a6ee
Trond Norbye <trond.norbye@gmail.com>
2012-05-27 00:18:38 +0200

    CBD-291: always prefer canonical versions of autotools
    Change-Id: Ibf01b0dfd2c7e27adb038443d962be5105972276
    Reviewed-on: http://review.couchbase.org/16446
    Tested-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- config/autorun.sh
+++ config/autorun.sh
@@ -56,14 +56,14 @@ if test x$LIBTOOLIZE = x; then
 fi
 
 if test x$ACLOCAL = x; then
-  ACLOCAL=`locate_binary aclocal-1.11 aclocal-1.10 aclocal`
+  ACLOCAL=`locate_binary aclocal aclocal-1.11 aclocal-1.10`
   if test x$ACLOCAL = x; then
     die "Did not find a supported aclocal"
   fi
 fi
 
 if test x$AUTOMAKE = x; then
-  AUTOMAKE=`locate_binary automake-1.11 automake-1.10 automake`
+  AUTOMAKE=`locate_binary automake automake-1.11 automake-1.10`
   if test x$AUTOMAKE = x; then
     die "Did not find a supported automake"
   fi
